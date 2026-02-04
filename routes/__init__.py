3318863f1095c6b5c62f6a5ebb4c0e550b7c2665
= <=>
2026-02-04 11:11:37 +0530

    Initialized the project and setup the home page.

---
+++ routes/__init__.py
@@ -0,0 +1,8 @@
+'''coderadi &bull; Routers initializer file for the Project.'''
+
+# ? IMPORTING ROUTE-MANAGERS
+from routes.app import app
+
+# * FUNCTION TO BIND ROUTERS WITH THE SERVER
+def bind_routers(server):
+    server.register_blueprint(app)
